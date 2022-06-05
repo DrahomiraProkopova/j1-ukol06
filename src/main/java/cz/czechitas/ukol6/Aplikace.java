de844ce23db79f67fb89183852f6fb7ce7ebd821
Drahomira Prokopova <prokopova.drahomira@gmail.com>
2022-06-05 19:43:07 +0200

    Ukol 6

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -10,6 +10,22 @@ import java.awt.event.ActionEvent;
 
 public class Aplikace extends JFrame {
 
+    int Husy;
+    int Králíci;
+
+
+    private JLabel HusyLabel;
+    private JLabel KrálíciLabel;
+    private JLabel Počet_hlavLabel;
+    private JLabel Počet_nohouLabel;
+    private JTextField HusyField;
+    private JTextField KrálíciField;
+    private JTextField Počet_hlavField;
+    private JTextField Počet_nohouField;
+
+    private JButton VypočítatButton;
+
+
     public static void main(String[] args) {
         FlatLightLaf.setup();
         new Aplikace().start();
@@ -32,8 +48,58 @@ public class Aplikace extends JFrame {
         setMinimumSize(new Dimension(250, 200));
 
         //TODO implementovat formulář podle zadání
+        HusyField = new JTextField();
+        HusyLabel = new JLabel("Husy");
+        HusyLabel.setDisplayedMnemonic('H');
+        HusyLabel.setLabelFor(HusyField);
+        add(HusyLabel);
+        add(HusyField);
+
+        KrálíciField = new JTextField();
+        KrálíciLabel = new JLabel("Králíci");
+        KrálíciLabel.setDisplayedMnemonic('K');
+        KrálíciLabel.setLabelFor(KrálíciField);
+        add(KrálíciLabel);
+        add(KrálíciField);
+
+        add(createButtonBar(), "span");
+
+        Počet_hlavField = new JTextField();
+        Počet_hlavField.setEnabled(false);
+        Počet_hlavLabel = new JLabel("Počet hlav");
+        Počet_hlavLabel.setLabelFor(Počet_hlavField);
+        add(Počet_hlavLabel);
+        add(Počet_hlavField);
+
+        Počet_nohouField = new JTextField();
+        Počet_nohouField.setEnabled(false);
+        Počet_nohouLabel = new JLabel("Počet nohou");
+        Počet_nohouLabel.setLabelFor(Počet_nohouField);
+        add(Počet_nohouLabel);
+        add(Počet_nohouField);
 
         pack();
+
+        VypočítatButton.addActionListener(this::handleVypočítat);
     }
 
+    private JPanel createButtonBar() {
+        VypočítatButton = new JButton("Vypočítat");
+        VypočítatButton.setMnemonic('V');
+
+        JPanel buttonBar = new JPanel(new MigLayout(null, "[right, grow]"));
+        buttonBar.add(VypočítatButton);
+        return buttonBar;
+    }
+
+    private void handleVypočítat(ActionEvent actionEvent) {
+        String husyFieldText = HusyField.getText();
+        Husy = Integer.valueOf(husyFieldText);
+        String králíciFieldText = KrálíciField.getText();
+        Králíci = Integer.valueOf(králíciFieldText);
+        int pocetHlav = Husy + Králíci;
+        int pocetNoh = (Husy * 2) + (Králíci * 4);
+        Počet_hlavField.setText(Integer.toString(pocetHlav));
+        Počet_nohouField.setText(Integer.toString(pocetNoh));
+    }
 }
